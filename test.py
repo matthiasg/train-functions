723886b4bce5805ccc9601eb29731d7bb5901b8b
Matthias Goetzke <m.goetzke@curasystems.de>
2023-07-19 17:06:50 +0200

    V3

--- test.py
+++ test.py
@@ -4,8 +4,11 @@ import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 
+print('TORCH', torch.__version__)
 
-TRAINING_EPOCHS = 5_000
+TRAINING_EPOCHS = 25_000
+TRAINING_SAMPLES_OVER_FUNCTION = 15
+TEST_SAMPLES_OVER_FUNCTION = 100
 
 loss_values = []
 
@@ -28,13 +31,19 @@ class Net(nn.Module):
         self.relu2 = nn.ReLU()
         self.fc3 = nn.Linear(hidden_size, hidden_size)
         self.relu3 = nn.ReLU()
-        self.fc4 = nn.Linear(hidden_size, output_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.relu4 = nn.ReLU()
+        self.fc5 = nn.Linear(hidden_size, hidden_size)
+        self.relu5 = nn.ReLU()
+        self.fc6 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         out = self.relu1(self.fc1(x))
         out = self.relu2(self.fc2(out))
-        out = self.relu3(self.fc3(out))
-        out = self.fc4(out)
+        # out = self.relu3(self.fc3(out))
+        # out = self.relu4(self.fc4(out))
+        # out = self.relu5(self.fc5(out))
+        out = self.fc6(out)
         return out
 
 # Instantiate the network
@@ -46,7 +55,7 @@ criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 
 # Generate some training data
-x_train = np.linspace(-15, 30, 10).reshape(-1, 1).astype('float32')
+x_train = np.linspace(-15, 30, TRAINING_SAMPLES_OVER_FUNCTION).reshape(-1, 1).astype('float32')
 # y_train = (2*x_train + 1).astype('float32')  # Linear function
 y_train = (3*x_train**2 + 2*x_train + 1).astype('float32')  # 2nd degree polynomial
 # y_train = (9*x_train**3 + -3*x_train**2 + 2*x_train + 1).astype('float32')  # 3rd degree polynomial
@@ -55,7 +64,7 @@ y_train = (3*x_train**2 + 2*x_train + 1).astype('float32')  # 2nd degree polynom
 train_data = [(x_train, y_train)] #, (x_train, y_train_poly2), (x_train, y_train_poly3)]
 
 # Generate some test data
-x_test = np.linspace(-30, 20, 100).reshape(-1, 1).astype('float32')
+x_test = np.linspace(-30, 20, TEST_SAMPLES_OVER_FUNCTION).reshape(-1, 1).astype('float32')
 # y_test = (2*x_test + 1).astype('float32')  # Linear function
 y_test = (3*x_test**2 + 2*x_test + 1).astype('float32')  # 2nd degree polynomial
 # y_test = (9*x_test**3 + -3*x_test**2 + 2*x_test + 1).astype('float32')  # 3rd degree polynomial
@@ -130,16 +139,16 @@ for x_test, y_test in test_data:
 y_pred = net(x_test).detach().cpu().numpy()
 
 # find the minimum and maximum values of x and y for the data and predictions
-x_min = min(x_train.min(), x_test.min())
-x_max = max(x_train.max(), x_test.max())
-y_min = min(y_train.min(), y_test.min(), y_pred.min())
-y_max = max(y_train.max(), y_test.max(), y_pred.max())
+x_min = min(x_train.cpu().min(), x_test.cpu().min())
+x_max = max(x_train.cpu().max(), x_test.cpu().max())
+y_min = min(y_train.cpu().min(), y_test.cpu().min(), y_pred.min())
+y_max = max(y_train.cpu().max(), y_test.cpu().max(), y_pred.max())
 
 # create a figure and a set of subplots
 fig, axs = plt.subplots(2)
 
 # plot the training data
-axs[0].plot(x_train, y_train, 'b.', label='Training data')
+axs[0].plot(x_train.cpu(), y_train.cpu(), 'b.', label='Training data')
 axs[0].set_title('Training Data')
 axs[0].set_xlabel('x')
 axs[0].set_ylabel('y')
@@ -147,8 +156,8 @@ axs[0].set_xlim(x_min, x_max)
 axs[0].set_ylim(y_min, y_max)
 
 # plot the test data and the network's predictions
-axs[1].plot(x_test, y_test, 'r.', label='Test data')
-axs[1].plot(x_test, y_pred, 'g-', label='Predicted')
+axs[1].plot(x_test.cpu(), y_test.cpu(), 'r.', label='Test data')
+axs[1].plot(x_test.cpu(), y_pred, 'g-', label='Predicted')
 axs[1].set_title('Test Data and Predictions')
 axs[1].set_xlabel('x')
 axs[1].set_ylabel('y')
